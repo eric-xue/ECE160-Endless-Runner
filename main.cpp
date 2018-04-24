7e3a409897bac890c61ea0aead6ae21caeaa2909
bchung2017 <chung3@cooper.edu>
2018-04-24 18:42:39 -0400

    added enemy object

--- main.cpp
+++ main.cpp
@@ -15,31 +15,62 @@ int main()
 	window.setFramerateLimit(30);
 	bool isjumped = false;
 	bool groundcollide;
+
 	sf::RectangleShape ground(sf::Vector2f(1024.0f, 150.0f));
 	ground.setOrigin(512.0f, 25.0f);
 	ground.setFillColor(sf::Color::Black);
 	ground.setPosition(512.0f, 512.0f);
+
 	sf::Clock clock;
 	float Player_dx = 0.0f;
 	float Player_dy = 0.0f;
 	float player_a = 80.0f;
-	window.setFramerateLimit(60);
 	
+	window.setFramerateLimit(60);
 	float dt;
+
+	//===========================
+	//===========================
+	//NEW CODE INITIALIZING ENEMY
+	sf::RectangleShape enemy1(sf::Vector2f(100.0f, 100.0f));
+	sf::Texture enemytexture;
+	enemytexture.loadFromFile("enemy.jpg");
+	enemy1.setTexture(&enemytexture);
+	enemy1.setOrigin(50.0f, 50.0f);
+	enemy1.setPosition(700.0f, 437.0f);
+	bool enemycollide;
+	float Enemy_dx = 0.0f;
+	//NEW CODE INITIALIZING ENEMY 
+	//===========================
+	//===========================
 
 	while (window.isOpen())
 	{
 		sf::Event evnt;
 		groundcollide = player.getGlobalBounds().intersects(ground.getGlobalBounds());
-		dt = clock.restart().asSeconds(); 
+		dt = clock.restart().asSeconds();
 
 		window.clear(sf::Color::White);
 		window.draw(player);
 		window.draw(ground);
+		window.draw(enemy1);
 		window.display();
-
-
 		std::cout << "beginning dy: " << Player_dy << std::endl;
+
+		player.move(Player_dx, Player_dy);
+
+		//===========================
+		//ENEMY DRAW CODE
+		enemycollide = enemy1.getGlobalBounds().intersects(player.getGlobalBounds());
+		enemy1.move(Enemy_dx, 0);
+		if (enemycollide)
+		{
+			std::cout << "colliding with enemy" << std::endl;
+			Player_dy = Player_dy;
+			Player_dx = 0;
+			player.setPosition(150, 437);
+		}
+
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) && groundcollide)
 		{
@@ -61,21 +92,21 @@ int main()
 			Player_dy = Player_dy + player_a * dt;
 			std::cout << "NOW VELOCITY" << Player_dy << std::endl;
 		}
+
 		
-		player.move(Player_dx, Player_dy);
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
 		{
 			if (player.getPosition().x >= 25.0f)
 				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LShift))
 				{
-					if(groundcollide)
+					if (groundcollide)
 						player.move(-15.0f, 0.0f);
 					else
 						Player_dx += -1.5f;
 				}
 				else
-					if(groundcollide)
+					if (groundcollide)
 						player.move(-10.0f, 0.0f);
 					else
 						Player_dx += -1.0f;
@@ -112,8 +143,18 @@ int main()
 			player.setPosition(player.getPosition().x, 438.0f);
 		}
 
+
+
+
+
+		
 	}
 
 
 	return 0;
+}
+
+
+void playerMove() {
+
 }
